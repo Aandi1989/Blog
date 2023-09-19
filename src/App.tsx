345d2c1039b125a8543fbca7bc232d2313c2f47c
Alexender Miadzvedzeu <>
2023-09-19 23:21:15 +0300

    answers

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Provider } from "react-redux";
 import { store } from "@core/store";
 import { MyThemeProvider } from "@shared/components";
 import { RouterProvider } from "react-router-dom";
+import { useRoutesWithUserRole } from "@shared/components/app-routes/app-routes.component";
 
 function App() {
+  const routerWithRoles = useRoutesWithUserRole();
   return (
     // <BrowserRouter>
     <MyThemeProvider>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <RouterProvider router={routerWithRoles} />
       </Provider>
     </MyThemeProvider>
     // </BrowserRouter>
--- src/shared/components/app-routes/app-routes.component.tsx
+++ src/shared/components/app-routes/app-routes.component.tsx
@@ -1,5 +1,9 @@
-import { Route, Routes, createBrowserRouter } from "react-router-dom";
+import { Route, Routes, createBrowserRouter, useRouteError } from "react-router-dom";
+
 import { routeConfig } from "./routeConfig";
+import { UsersPage } from "@pages/users";
+import { UserPage } from "@pages/user";
+import { HomePage } from "@pages/homepage";
 
 const Router: React.FC = () => {
   return (
@@ -16,12 +20,89 @@ const Router: React.FC = () => {
 };
 
 const router = createBrowserRouter([
+  // TODO: Тут была ошибка (все по документации)
   {
     path: "/",
-    element: <Router />,
+    element: <HomePage />,
+  },
+  {
+    path: "/users",
+    element: <UsersPage />,
+  },
+  {
+    path: "users/hui",
+    element: <div children="hui" />,
+  },
+  {
+    path: "users/:id",
+    element: <UserPage />,
   },
 ]);
 
 export default router;
 
 // как сюда прикрутить private route и роутинг по ролям??
+// Изи. Чисто методы js
+// PSEUDOCODE
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        {/* @ts-ignore */}
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
+
+const adminRoutes = [
+  {
+    path: "/users",
+    element: <UsersPage />,
+  },
+  {
+    path: "users/hui",
+    element: <div children="hui" />,
+  },
+  {
+    path: "users/:id",
+    element: <UserPage />,
+  },
+];
+
+const loxEbaniRoutes = [
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <h1>404 No page. blya</h1>,
+  },
+  {
+    path: "users/hui",
+    element: <div children="hui" />,
+  },
+];
+
+enum UserRoles {
+  Admin = "ADMIN",
+  LohEbani = "LOX_EBANII",
+}
+
+const useProfile = () => {
+  return {
+    firstName: "Hui",
+    lastName: "Blyat",
+    role: UserRoles.LohEbani,
+  };
+};
+
+export const useRoutesWithUserRole = () => {
+  const { role } = useProfile();
+  const routes = [...loxEbaniRoutes, ...(role === UserRoles.Admin ? adminRoutes : [])];
+  return createBrowserRouter(routes);
+};
